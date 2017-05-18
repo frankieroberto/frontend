1b427a5ddb645c399271f83823995d2c8be3e5b1
Carlos Vilhena <carlosvilhena@gmail.com>
2017-05-18 15:55:32 +0000

    Remove Start button on register to vote pages

--- app/controllers/transaction_controller.rb
+++ app/controllers/transaction_controller.rb
@@ -7,6 +7,12 @@ class TransactionController < ApplicationController
   before_action :deny_framing
 
   def show
+    @register_to_vote_slugs = %w(
+      register-to-vote
+      register-to-vote-crown-servants-british-council-employees
+      register-to-vote-armed-forces
+      cofrestru-i-bleidleisio
+    )
   end
 
   def jobsearch
